af0705fe7e00b60fbf1ba9bee77f2418288702c9
petrecirceagwork <petrecirceagwork@gmail.com>
2024-05-31 17:12:38 +0000

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+nextConfig.images = {
+    domains: ['picsum.photos'], // Replace with your allowed domains
+  };
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from '@chakra-ui/react'
+
+
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  );
+}
